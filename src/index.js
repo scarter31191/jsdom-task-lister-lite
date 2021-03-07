28340631c37408d32bc2de4251545a5c2b2e836d
scarter31191 <scarter31191@gmaile.com>
2021-03-07 09:54:31 -0500

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById('create-task-form')
+  // console.log(newTaskForm)
+
+  const newTaskDescription = document.getElementById('new-task-description')
+  // console.log(newTaskDescription)
+
+  const newTaskUl = document.getElementById('tasks')
+  // console.log(newTaskUl)
+
+  newTaskForm.addEventListener("submit", createNewTask)
+
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById('new-task-description');
+  console.log(newTaskDescription)
+
+  const newTask = document.createElement('li')
+
+  newTask.innerHTML = newTaskDescription.value
+  console.log(newTask.innerHTML)
+
+  appendNewTask(newTask);
+  event.target.reset();
+
+}
+
+const appendNewTask = task => {
+  document.getElementById('tasks').appendChild(task);
+};
+
+// const newTaskUl.addEventListener('click', (createNewTask) => {
+//   if(createNewTask.target === 'BUTTON') {
+//     newTask.deleteTask(createNewTask.target.dataset.description);
+//     renderApp();
+//   }
+// });
